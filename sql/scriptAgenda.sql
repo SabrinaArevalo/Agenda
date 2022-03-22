0aed6cc63a8ddaca6ee9be6a75859d7a1231107d
SabrinaArevalo <sabriarevalo123@gmail.com>
2022-03-22 12:34:40 -0300

    agregue las tablas

--- sql/scriptAgenda.sql
+++ sql/scriptAgenda.sql
@@ -7,3 +7,39 @@ CREATE TABLE `personas`
   `Telefono` varchar(20) NOT NULL,
   PRIMARY KEY (`idPersona`)
 );
+
+CREATE TABLE `domicilio`
+(
+	`idDomicilio` varchar(45) NOT NULL AUTO_INCREMENT,
+     `calle` varchar(45) NOT NULL,
+     `altura` int(5) NOT NULL, 
+     `piso` int(10) NOT NULL,
+     `depto` int(10) NOT NULL, 
+     `localidad` varchar(45) NOT NULL,
+	 `Provincia` varchar(50) NOT NULL,
+     `Pais` varchar(50) NOT NULL,
+			PRIMARY KEY (`idDomicilio`)
+);
+
+CREATE TABLE `localidades` 
+(
+	`idLocalidad` int(11) NOT NULL AUTO_INCREMENT,
+	`Nombre` varchar(45) NOT NULL,
+			PRIMARY KEY (`idLocalidad`)
+);
+
+CREATE TABLE `provincias` (
+  `idProvincia` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) NOT NULL,
+  `idLocalidad` int(11) NOT NULL,
+    FOREIGN KEY (`idLocalidad`) REFERENCES `localidades`(`idLocalidad`) ON UPDATE CASCADE ,
+          PRIMARY KEY (`idProvincia`)
+);
+
+CREATE TABLE `paises` (
+  `idPais` int(11) NOT NULL  AUTO_INCREMENT,
+  `Nombre` varchar(45) NOT NULL,
+  `idProvincia` int(11) NOT NULL,
+        FOREIGN KEY (`idProvincia`) REFERENCES `provincias`(`idProvincia`) ON UPDATE CASCADE ,
+              PRIMARY KEY (`idPais`)
+) 
